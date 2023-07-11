cacd4916a6c81dc3294ce00badf7f1a89bdba244
GITUPMORE <jackylovecanada@gmail.com>
2023-07-11 13:07:36 -0500

    Demo6 regular update 2023/7/11

--- app/src/main/java/com/example/workdemo101/LogInActivity.java
+++ app/src/main/java/com/example/workdemo101/LogInActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +18,8 @@ import android.widget.Toast;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 
 public class LogInActivity extends AppCompatActivity {
@@ -42,22 +45,51 @@ public class LogInActivity extends AppCompatActivity {
             {
                 String email = Email.getText().toString();
                 String pass = Password.getText().toString();
-                if(email.equals("Jackylove@123.com") && pass.equals("123"))
+                Connection connection = connection();
+
+                if(TextUtils.isEmpty(email))
                 {
-                    Toast.makeText(LogInActivity.this, "Welcome", Toast.LENGTH_SHORT);
-                    startActivity(new Intent(LogInActivity.this,Chat.class));
+                    Email.setError("Email is Required!");
+                    return;
                 }
-                else
+
+                if(TextUtils.isEmpty(pass))
                 {
-                    Toast.makeText(LogInActivity.this, "Invalid", Toast.LENGTH_LONG);
+                    Password.setError("Password is Required!");
+                    return;
                 }
+
+                try {
+                    if (connection != null)
+                    {
+                        String sql="select * from UserInfo where Email = '" + Email.getText().toString() + "'";
+                        Statement st=connection.createStatement();
+                        ResultSet rs = st.executeQuery(sql);
+
+                        while(rs.next())
+                        {
+                            if (email != rs.getString(1) && pass != rs.getString(2))
+                            {
+                                startActivity(new Intent(LogInActivity.this,Chat.class));
+                            }
+                            else
+                            {
+                                Toast.makeText(LogInActivity.this, "Invalid", Toast.LENGTH_LONG);
+                            }
+                        }
+                    }
+                }
+                catch (Exception exception){
+
+                }
+
             }
         });
 }
     @SuppressLint("NewApi")
     public Connection connection(){
         Connection conn = null;
-        String ip="192.168.0.104", port="50308" , username="WorkDemo", password="123" , databasename="WrokDemo" ;
+        String ip="10.162.55.120", port="50308" , username="WorkDemo", password="123" , databasename="WrokDemo" ;
         StrictMode.ThreadPolicy tp = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(tp);
         try
--- app/src/main/java/com/example/workdemo101/RegisterActivity.java
+++ app/src/main/java/com/example/workdemo101/RegisterActivity.java
@@ -64,7 +64,7 @@ public class RegisterActivity extends AppCompatActivity {
     @SuppressLint("NewApi")
     public Connection connection(){
         Connection conn = null;
-        String ip="192.168.0.104", port="50308" , username="WorkDemo", password="123" , databasename="WrokDemo" ;
+        String ip="10.162.55.120", port="50308" , username="WorkDemo", password="123" , databasename="WrokDemo" ;
         StrictMode.ThreadPolicy tp = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(tp);
         try
